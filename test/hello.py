bdbccd5e15f76b9dbac7a70f6516f5e36eaa4a13
misaAle <misaelcasarez93@gmail.com>
2022-11-20 16:56:50 -0800

    Editing code for our example

--- test/hello.py
+++ test/hello.py
@@ -1,6 +1,7 @@
-print("Hello world, from python.")
+print("Hello world.")
 print("First change.")
 print("Second change.")
 
 for counter in range(1,11):
     print(counter)
+print("This is another change.")
